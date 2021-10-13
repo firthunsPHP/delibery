fe58c81f8eb3699405cb29e2ba650ff72c3c054c
firthunsps@gmail.com <firthunsps@gmail.com>
2021-10-13 18:10:40 +0200

    dia 11....

--- src/Controller/CategoriaController.php
+++ src/Controller/CategoriaController.php
@@ -6,6 +6,7 @@ namespace App\Controller;
 
 use App\Entity\Categoria;
 
+use App\Repository\CategoriaRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -19,53 +20,81 @@ class CategoriaController extends  AbstractController
 // composer require logger -> instalacion realizada
 
     private $logger;
-    public  function  __construct(LoggerInterface $logger){ // constructor
+    private $catRepository;
+    public  function  __construct(LoggerInterface $logger, CategoriaRepository $categoriaRepository){ // constructor
 
         $this->logger = $logger;
+        $this->catRepository = $categoriaRepository;
     }
 
     // Con la anotación Route establecemos el endpoint
+//    /**
+//     * @Route("/categoria/list" , name="categoria_list" )
+//     */
+
+// Método asociado al endpoint, siempre tiene que devolver un respones
+//    public  function listCategorias(Request $request, CategoriaRepository $categoriaRepository ){
+//    public  function listCategorias(Request $request, CategoriaRepository $categoriaRepository ){
+////        $response = new Response();
+////        $response->setContent('<div> Hola Mundo</div>');
+////        return $response;
+//
+//        $categoria = $categoriaRepository->findAll();
+//
+//        // antes de montar el json, capturamos el contenido del request
+//        //        127.0.0.1:8000/categoria/list?categoria=Indu
+//
+//
+//        $categoria = $request->get('categoria');
+//        $response = new JsonResponse();
+//        $response-> setData([
+//            'success' => true,
+//            'data' =>  [
+//                [
+//                    'id' => 1,
+//                    'categoria' => 'Japones'
+//                ],
+//                [
+//                    'id' => 2,
+//                    'categoria' => 'Italiano'
+//                ],
+//                [
+//                    'id' => 2,
+//                    'categoria' => $categoria
+//                ],
+//            ]
+//        ]);
+//
+//        $this->logger->info('List categorias matched!!!!');
+////     el resultado lo veriamos en el directorio var>log> dev.log
+//        return $response;
+//    }
     /**
      * @Route("/categoria/list" , name="categoria_list" )
      */
 
-// Método asociado al endpoint, siempre tiene que devolver un respones
-    public  function listCategorias(Request $request, CategoriaRepository $categoriaRepository ){
-//        $response = new Response();
-//        $response->setContent('<div> Hola Mundo</div>');
-//        return $response;
+    public function  listCategorias(){
+    // end point que nos devuelve to.do el contenido de la categoria
+        $categorias = $this->catRepository->findAll();
 
-        $categoria = $categoriaRepository->findAll();
+        // la info la tiene que devolver en json..., asi que
+        // creamos un array []
+        $categoriasAsArray = [];
+        foreach ( $categorias as $cat){
+            $categoriasAsArray[] = [
+              'id' => $cat->getId(),
+              'categoria'=> $cat->getCategoria()
+            ];
+        }
 
-        // antes de montar el json, capturamos el contenido del request
-        //        127.0.0.1:8000/categoria/list?categoria=Indu
 
-
-        $categoria = $request->get('categoria');
         $response = new JsonResponse();
-        $response-> setData([
-            'success' => true,
-            'data' =>  [
-                [
-                    'id' => 1,
-                    'categoria' => 'Japones'
-                ],
-                [
-                    'id' => 2,
-                    'categoria' => 'Italiano'
-                ],
-                [
-                    'id' => 2,
-                    'categoria' => $categoria
-                ],
-            ]
+        $response->setData([
+              'succes' => true,
+              'data' => $categoriasAsArray
         ]);
-
-        $this->logger->info('List categorias matched!!!!');
-//     el resultado lo veriamos en el directorio var>log> dev.log
         return $response;
     }
-
 
     /**
      * @Route ("/categoria", name="create_categoria")
